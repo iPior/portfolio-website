d6dfd02a7f4c82a9aea008dced8d2e77cf3740e6
Piotr Szaran <szaranpiotr@gmail.com>
2025-04-08 18:18:03 -0400

    safari bug on header

--- components/sections/hero.tsx
+++ components/sections/hero.tsx
@@ -4,7 +4,7 @@ export function Hero() {
   return (   
       <section id="hero" className="mx-auto w-full">
         <p className="text-md md:text-xl text-muted-foreground w-full font-bold mt-18 md:mt-24 px-4">Hi, I&apos;m</p>
-        <BorderWrapper className="px-3 pb-2 xs:pb-3 sm:pb-4 lg:px-1 lg:pb-6 font-extrabold text-7xl xs:text-8xl sm:text-9xl lg:text-hero uppercase">
+        <BorderWrapper className="px-3 py-2 xs:pb-3 sm:pb-4 lg:px-1 lg:pb-6 font-extrabold text-7xl xs:text-8xl sm:text-9xl lg:text-hero uppercase">
           <h1 className="tracking-tight flex items-center leading-15 xs:leading-20 sm:leading-25 lg:leading-35">Piotr</h1>
           <h1 className="tracking-tight flex items-center leading-15 xs:leading-20 sm:leading-25 lg:leading-35">Szaran</h1>
         </BorderWrapper>
