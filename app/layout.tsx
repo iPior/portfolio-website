22861f53b412f26d11c3eefd72c1e66957c46868
Piotr Szaran <szaranpiotr@gmail.com>
2025-04-12 00:14:35 -0400

    open graph image added

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,6 +17,9 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "Hi, I'm Piotr Szaran",
   description: "A Web Developer based in Toronto.",
+  openGraph: {
+    images: "https://www.piotrszaran.com/images/og-image.png",
+  },
 };
 
 export default function RootLayout({
